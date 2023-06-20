fa7158e15822508c548135230b77c543a3ca14d3
SonderMX <sonder-mx@outlook.com>
2023-06-20 19:46:59 +0800

    更新token接口

--- da_token/serializers.py
+++ da_token/serializers.py
@@ -14,6 +14,7 @@ class DaTokenSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        token['email'] = user.email
         token['username'] = user.username
         token['is_staff'] = user.is_staff
         token['is_superuser'] = user.is_superuser
