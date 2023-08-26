f335eae5772c66a55d7e6b9aecb18a7a60e68621
SonderMX <sonder-mx@outlook.com>
2023-08-26 21:09:20 +0800

    da_redis 测试数据

---
+++ sql_test_data/da_redis.sql
@@ -0,0 +1,5 @@
+INSERT INTO redis_books(name, price, pub_date, publish) VALUES('三体', 99.99, NOW(), '小明出版社');
+INSERT INTO redis_books(name, price, pub_date, publish) VALUES('红楼梦', 199.99, NOW(), '小明出版社');
+INSERT INTO redis_books(name, price, pub_date, publish) VALUES('西游记', 65.65, NOW(), '小明出版社');
+INSERT INTO redis_books(name, price, pub_date, publish) VALUES('水浒传', 100.32, NOW(), '小明出版社');
+INSERT INTO redis_books(name, price, pub_date, publish) VALUES('三国演义', 88.99, NOW(), '小明出版社');
